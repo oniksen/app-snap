2f5b3a0a1f997aba9823e80b168741f6dbaeeb7e
oniksen <bardakov.roman99@gmail.com>
2025-10-22 21:06:16 +0300

    Кнопка открытия приложения
    Добавлена кнопка открытия выбранного приложения.

--- app/src/main/java/dev/oniksen/app_snap/presentation/pages/app_details/AppDetailsPage.kt
+++ app/src/main/java/dev/oniksen/app_snap/presentation/pages/app_details/AppDetailsPage.kt
@@ -1,12 +1,16 @@
 package dev.oniksen.app_snap.presentation.pages.app_details
 
+import android.content.Intent
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3ExpressiveApi
 import androidx.compose.material3.MaterialExpressiveTheme
 import androidx.compose.material3.Surface
@@ -15,6 +19,7 @@ import androidx.compose.material3.darkColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import dev.oniksen.app_snap.domain.model.AppInfo
@@ -26,6 +31,8 @@ fun AppDetailsPage(
     appInfo: AppInfo?,
 ) {
     if (appInfo != null) {
+        val localContext = LocalContext.current
+
         Column(
             modifier = modifier.fillMaxSize()
         ) {
@@ -33,6 +40,17 @@ fun AppDetailsPage(
             LineInfo("Версия:", appInfo.appVersion ?: "Неизвестно")
             LineInfo("Имя пакета:", appInfo.packageName)
             LineInfo("Контрольная сумма (sha-256):", appInfo.hashSum)
+            Spacer(modifier = Modifier.height(20.dp))
+            Button(
+                onClick = {
+                    val launchIntent = localContext.packageManager.getLaunchIntentForPackage(appInfo.packageName)
+                    if (launchIntent != null) {
+                        localContext.startActivity(launchIntent)
+                    }
+                }
+            ) {
+                Text("Открыть приложение")
+            }
         }
     } else {
         Box(
